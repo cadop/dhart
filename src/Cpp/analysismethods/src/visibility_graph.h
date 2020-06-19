cc882f6d5755e869a23c4cae49d6f49e042ed81c
Gemuele Aludino <g.aludino@gmail.com>
2020-06-19 19:57:40 -0400

    Begin code sample formatting adjustment for objloader, pathfinding, and visibility_graph. Many of them still need to be fixed for alignment -- but samples for those namespaces need to be tested and revised anyway.

--- src/Cpp/analysismethods/src/visibility_graph.h
+++ src/Cpp/analysismethods/src/visibility_graph.h
@@ -34,39 +34,41 @@ namespace HF {
 			/// <param name="input_nodes">reference to vector of Node for VisibilityGraph</param>
 			/// <param name="height">Desired height for the VisibilityGraph</param>
 
-			/// \code{.cpp}
-			/// using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
-			/// using HF::SpatialStructures::Node;			// needed for Node
-			/// using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
-			/// using HF::Geometry::Graph;					// needed for Graph
-			///
-			/// std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
-			///
-			/// // Create an EmbreeRayTracer.
-			/// // Note that LoadMeshObjects has two more arguments after plane_path -
-			///	//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
-			/// //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
-			///
-			/// EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
-			///
-			///	std::vector<Node> node_vec;			// Container of nodes
-			/// const int reserve_count = 100;		// Pre-defined reserve size
-			/// node_vec.reserve(reserve_count);	// Reserve reserve_count blocks for node_vec
-			///
-			/// // Construct reserve_count Node and insert each of them into node_vec
-			/// for (float i = -5.0; i < 5.0; i++) {
-			///		// runs 10 times
-			///		for (float j = -5; j < 5.0; j++) {
-			///			// runs 10 times
-			///			node_vec.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
-			///		}
-			/// }
-			///
-			///	// AllToAll constructs and returns a Graph 
-			/// // consisting of Node (from node_vec) that do not occlude each other
-			/// float desired_height = 2.0f;		// Height of graph
-			/// Graph graph = AllToAll(tracer, node_vec, desired_height);
-			/// \endcode
+			/*!
+			 \code{.cpp}
+			 using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
+			 using HF::SpatialStructures::Node;			// needed for Node
+			 using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
+			 using HF::Geometry::Graph;					// needed for Graph
+			
+			 std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
+			
+			 // Create an EmbreeRayTracer.
+			 // Note that LoadMeshObjects has two more arguments after plane_path -
+				//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
+			 //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
+			
+			 EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
+			
+				std::vector<Node> node_vec;			// Container of nodes
+			 const int reserve_count = 100;		// Pre-defined reserve size
+			 node_vec.reserve(reserve_count);	// Reserve reserve_count blocks for node_vec
+			
+			 // Construct reserve_count Node and insert each of them into node_vec
+			 for (float i = -5.0; i < 5.0; i++) {
+					// runs 10 times
+					for (float j = -5; j < 5.0; j++) {
+						// runs 10 times
+						node_vec.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
+					}
+			 }
+			
+				// AllToAll constructs and returns a Graph 
+			 // consisting of Node (from node_vec) that do not occlude each other
+			 float desired_height = 2.0f;		// Height of graph
+			 Graph graph = AllToAll(tracer, node_vec, desired_height);
+			 \endcode
+			*/
 			HF::SpatialStructures::Graph AllToAll(
 				HF::RayTracer::EmbreeRayTracer& ert,
 				const std::vector<HF::SpatialStructures::Node>& input_nodes,
@@ -80,48 +82,50 @@ namespace HF {
 			/// <param name="input_nodes">reference to vector of Node for VisibilityGraph</param>
 			/// <param name="height">Desired height for the VisibilityGraph</param>
 
-			/// \code{.cpp}
-			/// using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
-			/// using HF::SpatialStructures::Node;			// needed for Node
-			/// using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
-			/// using HF::Geometry::Graph;					// needed for Graph
-			///
-			/// std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
-			///
-			/// // Create an EmbreeRayTracer.
-			/// // Note that LoadMeshObjects has two more arguments after plane_path -
-			///	//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
-			/// //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
-			/// 
-			/// EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
-			///
-			///	std::vector<Node> node_vec_0;		// First container of Node
-			/// std::vector<Node> node_vec_1;		// Second container of Node
-			///
-			/// const int reserve_count = 100;		// Pre-defined reserve size
-			/// node_vec_0.reserve(reserve_count);	// Reserve reserve_count blocks for both vectors
-			/// node_vec_1.reserve(reserve_count);
-			///
-			/// // 0) Construct reserve_count Node and insert each of them into node_vec_0
-			/// for (float i = -5.0; i < 5.0; i++) {
-			///		// runs 10 times
-			///		for (float j = -5.0; j < 5.0; j++) {
-			///			// runs 10 times
-			///			node_vec_0.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
-			///		}
-			/// }
-			///
-			/// // 1) Construct reserve_count Node and insert each of them into node_vec_1
-			/// for (float i = 0.0; i < 10.0; i++) {
-			///		for (float j = 0.0; j < 10.0; j++) {
-			///			node_vec_1.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
-			///		}
-			///
-			///	// GroupToGroup constructs and returns a Graph consisting of Node
-			/// // (between node_vec_0 and node_vec_1) such that the nodes do not occlude each other
-			/// float desired_height = 2.0f;		// Height of graph
-			/// Graph graph = GroupToGroup(tracer, node_vec_0, node_vec_1, desired_height);
-			/// \endcode
+			/*!
+			 \code{.cpp}
+			 using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
+			 using HF::SpatialStructures::Node;			// needed for Node
+			 using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
+			 using HF::Geometry::Graph;					// needed for Graph
+			
+			 std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
+			
+			 // Create an EmbreeRayTracer.
+			 // Note that LoadMeshObjects has two more arguments after plane_path -
+				//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
+			 //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
+			 
+			 EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
+			
+				std::vector<Node> node_vec_0;		// First container of Node
+			 std::vector<Node> node_vec_1;		// Second container of Node
+			
+			 const int reserve_count = 100;		// Pre-defined reserve size
+			 node_vec_0.reserve(reserve_count);	// Reserve reserve_count blocks for both vectors
+			 node_vec_1.reserve(reserve_count);
+			
+			 // 0) Construct reserve_count Node and insert each of them into node_vec_0
+			 for (float i = -5.0; i < 5.0; i++) {
+					// runs 10 times
+					for (float j = -5.0; j < 5.0; j++) {
+						// runs 10 times
+						node_vec_0.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
+					}
+			 }
+			
+			 // 1) Construct reserve_count Node and insert each of them into node_vec_1
+			 for (float i = 0.0; i < 10.0; i++) {
+					for (float j = 0.0; j < 10.0; j++) {
+						node_vec_1.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
+					}
+			
+				// GroupToGroup constructs and returns a Graph consisting of Node
+			 // (between node_vec_0 and node_vec_1) such that the nodes do not occlude each other
+			 float desired_height = 2.0f;		// Height of graph
+			 Graph graph = GroupToGroup(tracer, node_vec_0, node_vec_1, desired_height);
+			 \endcode
+			*/
 			HF::SpatialStructures::Graph GroupToGroup(
 				HF::RayTracer::EmbreeRayTracer& ert,
 				const std::vector<HF::SpatialStructures::Node> & from,
@@ -137,41 +141,43 @@ namespace HF {
 			/// <param name="height">Desired height for the VisibilityGraph</param>
 			/// <param name="cores">Core count for the graph</param>
 
-			/// \code{.cpp}
-			/// using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
-			/// using HF::SpatialStructures::Node;			// needed for Node
-			/// using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
-			/// using HF::Geometry::Graph;					// needed for Graph
-			///
-			/// std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
-			///
-			/// // Create an EmbreeRayTracer.
-			/// // Note that LoadMeshObjects has two more arguments after plane_path -
-			///	//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
-			/// //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
-			/// 
-			/// EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
-			///
-			///	std::vector<Node> node_vec;			// Container of nodes
-			/// const int reserve_count = 100;		// Pre-defined reserve size
-			/// node_vec.reserve(reserve_count);	// Reserve reserve_count blocks for node_vec
-			///
-			/// // Construct reserve_count Node and insert each of them into node_vec
-			/// for (float i = -5.0; i < 5.0; i++) {
-			///		// runs 10 times
-			///		for (float j = -5; j < 5.0; j++) {
-			///			// runs 10 times
-			///			node_vec.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
-			///		}
-			/// }
-			///
-			///	// AllToAllUndirected constructs and returns a Graph 
-			/// // consisting of Node (from node_vec) that do not occlude each other
-			/// float desired_height = 2.0f;		// Height of graph
-			/// int core_count = 4;					// For omp_set_num_threads(int num_threads), CPU core count
-			///
-			/// Graph graph = AllToAllUndirected(tracer, node_vec, desired_height, core_count);
-			/// \endcode
+			/*!
+			 \code{.cpp}
+			 using HF::RayTracer::EmbreeRayTracer;		// needed for EmbreeRayTracer
+			 using HF::SpatialStructures::Node;			// needed for Node
+			 using HF::Geometry::LoadMeshObjects;		// needed for LoadMeshObjects
+			 using HF::Geometry::Graph;					// needed for Graph
+			
+			 std::string plane_path = "plane.obj";		// path to an OBJ file for a flat plane
+			
+			 // Create an EmbreeRayTracer.
+			 // Note that LoadMeshObjects has two more arguments after plane_path -
+				//		an enum HF::Geometry::GROUP_METHOD (defaults to GROUP_METHOD::ONLY_FILE)
+			 //		a bool (defaults to true, used to convert OBJ coordinate system to Rhino coordinates)
+			 
+			 EmbreeRayTracer tracer(LoadMeshObjects(plane_path));
+			
+				std::vector<Node> node_vec;			// Container of nodes
+			 const int reserve_count = 100;		// Pre-defined reserve size
+			 node_vec.reserve(reserve_count);	// Reserve reserve_count blocks for node_vec
+			
+			 // Construct reserve_count Node and insert each of them into node_vec
+			 for (float i = -5.0; i < 5.0; i++) {
+					// runs 10 times
+					for (float j = -5; j < 5.0; j++) {
+						// runs 10 times
+						node_vec.emplace_back(Node(i, j, 0.0f));	// all Node ID default to -1
+					}
+			 }
+			
+				// AllToAllUndirected constructs and returns a Graph 
+			 // consisting of Node (from node_vec) that do not occlude each other
+			 float desired_height = 2.0f;		// Height of graph
+			 int core_count = 4;					// For omp_set_num_threads(int num_threads), CPU core count
+			
+			 Graph graph = AllToAllUndirected(tracer, node_vec, desired_height, core_count);
+			 \endcode
+			*/
 			HF::SpatialStructures::Graph AllToAllUndirected(
 				HF::RayTracer::EmbreeRayTracer& ert,
 				const std::vector<HF::SpatialStructures::Node>& nodes,
--- src/Cpp/pathfinding/src/boost_graph.h
+++ src/Cpp/pathfinding/src/boost_graph.h
@@ -17,15 +17,17 @@ namespace std {
 	/// </summary>
 	/// @tparam SizeT Integral type, such as size_t (implementation defined)
 
-	/// \code{.cpp}
-	///	std::size_t seed_in = 0;
-	/// std::size_t value_in = VALUE_USER_DEFINED;	// user-defined value for seed
-	///
-	///	// seed_in is passed by reference
-	///	std::array_hash_combine_impl<size_t>(seed_in, value_in);
-	///
-	///	// seed_in has now been given a value by array_hash_combine_impl
-	/// \endcode
+	/*!
+		\code{.cpp}
+			std::size_t seed_in = 0;
+			std::size_t value_in = VALUE_USER_DEFINED;	// user-defined value for seed
+	
+			// seed_in is passed by reference
+			std::array_hash_combine_impl<size_t>(seed_in, value_in);
+	
+			// seed_in has now been given a value by array_hash_combine_impl
+		\endcode
+	*/
 	template <typename SizeT>
 	inline void array_hash_combine_impl(SizeT& seed, SizeT value)
 	{
@@ -41,14 +43,16 @@ namespace std {
 		/// <param name="k">Reference to an array of coordinates (x, y, z)</param>
 		/// <returns>A seed, using std::array_has_combine_impl</returns>
 
-		/// \code{.cpp}
-		/// // Create coordinates, (x, y, z)
-		/// std::array<float, 3> arr = { 123.0, 456.1, 789.2 };
-		///
-		/// // Using the template specialization for std::hash (which takes a std::array<float, 3>),
-		/// // we retrieve a seed
-		/// std::size_t arr_hash = std::hash<std::array<float, 3>>(arr);
-		/// \endcode
+		/*!
+			\code{.cpp}
+				// Create coordinates, (x, y, z)
+				std::array<float, 3> arr = { 123.0, 456.1, 789.2 };
+		
+				// Using the template specialization for std::hash (which takes a std::array<float, 3>),
+				// we retrieve a seed
+				std::size_t arr_hash = std::hash<std::array<float, 3>>(arr);
+			\endcode
+		*/
 		inline std::size_t operator()(const std::array<float, 3>& k) const
 		{
 			size_t seed = std::hash<float>()(k[0]);
@@ -127,52 +131,56 @@ namespace HF {
 			/// <summary>
 			/// Create a new Boost graph from the given HF Graph
 			/// </summary>
-
-			/// \code{.cpp}
-			/// // In order to create a Boost graph, we must first create a
-			///	// HF::SpatialStructures::Graph instance first.
-			///
-			/// using HF::SpatialStructures::Node;
-			/// using HF::SpatialStructures::Graph;
-			///
-			/// // Create the nodes
-			/// Node node_0(12.0, 23.1, 34.2, 456);
-			/// Node node_1(45.3, 56.4, 67.5, 789);
-			/// Node node_2(55.5, 25.1, 85.2, 940);
-			///
-			///	// Create a container (vector) of nodes
-			/// std::vector<Node> nodes = { node_0, node_1, node_2 };
-			///
-			/// // Create matrices for edges and distances, edges.size() == distances().size()
-			///	std::vector<std::vector<int>> edges = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9 } };
-			/// std::vector<std::vector<float>> distances = { {12.0, 23.1, 45.2}, {67.3, 89.4, 98.5}, {76.6, 54.7, 32.8} };
-			///
-			///	// Now you can create a Graph - note that nodes, edges, and distances are passed by reference
-			/// Graph g(edges, distances, nodes);
-			///
-			/// // Passing Graph g to BoostGraph bg, by reference
-			/// BoostGraph bg(g);
-			/// \endcode
+			
+			/*!
+				\code{.cpp}
+					// In order to create a Boost graph, we must first create a
+					// HF::SpatialStructures::Graph instance first.
+			
+					using HF::SpatialStructures::Node;
+					using HF::SpatialStructures::Graph;
+			
+					// Create the nodes
+					Node node_0(12.0, 23.1, 34.2, 456);
+					Node node_1(45.3, 56.4, 67.5, 789);
+					Node node_2(55.5, 25.1, 85.2, 940);
+			
+					// Create a container (vector) of nodes
+					std::vector<Node> nodes = { node_0, node_1, node_2 };
+			
+					// Create matrices for edges and distances, edges.size() == distances().size()
+					std::vector<std::vector<int>> edges = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9 } };
+					std::vector<std::vector<float>> distances = { {12.0, 23.1, 45.2}, {67.3, 89.4, 98.5}, {76.6, 54.7, 32.8} };
+			
+					// Now you can create a Graph - note that nodes, edges, and distances are passed by reference
+					Graph g(edges, distances, nodes);
+			
+					// Passing Graph g to BoostGraph bg, by reference
+					BoostGraph bg(g);
+				\endcode
+			*/
 			BoostGraph(const HF::SpatialStructures::Graph& graph);
 
 			/// <summary>
 			/// Destroy the current Boost graph (uses compiler-generated implementation via 'default')
 			/// </summary>
-
-			/// \code{.cpp}
-			/// // Begin scope
-			/// {
-			///		// Create a HF::SpatialStructures::Graph g here first.
-			///
-			///		// Create a BoostGraph bg, from a Graph g (passing g by reference)
-			///		BoostGraph bg(g);
-			/// }
-			/// // End scope
-			///
-			///	// When bg goes out of scope, BoostGraph::~BoostGraph is called
-			/// // An explicit call to BoostGraph::~BoostGraph is also made when
-			/// // invoking operator delete on a (BoostGraph *)
-			/// \endcode
+			
+			/*!
+				\code{.cpp}
+					// Begin scope
+					{
+						// Create a HF::SpatialStructures::Graph g here first.
+			
+						// Create a BoostGraph bg, from a Graph g (passing g by reference)
+						BoostGraph bg(g);
+					}
+					// End scope
+			
+					// When bg goes out of scope, BoostGraph::~BoostGraph is called
+					// An explicit call to BoostGraph::~BoostGraph is also made when
+					// invoking operator delete on a (BoostGraph *)
+				\endcode
+			*/
 			~BoostGraph();
 		};
 	}
