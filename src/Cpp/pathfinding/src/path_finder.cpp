0a1d54109c1b631114a2111a039a20333a28259f
db368 <db368@njit.edu>
2020-06-25 15:01:55 -0400

    Move functions in pathfinder.cpp to Pathfinding Namespace
    This is purely for documentation purposes and has no impact on the code itself since they're not accessible outside of the .cpp anyway.

--- src/Cpp/pathfinding/src/path_finder.cpp
+++ src/Cpp/pathfinding/src/path_finder.cpp
@@ -27,128 +27,127 @@ using namespace HF::SpatialStructures;
 using namespace HF::Pathfinding;
 using std::vector;
 
-/// <summary>
-/// Holds a distance and predecessor matrix for the boost pathfinding algorithm
-/// </summary>
-struct DistPred {
-	std::vector<float> distance;
-	std::vector<vertex_descriptor> predecessor;
-
-	DistPred() {};
-
-	/// <summary>
-	/// Resize both starting arrays  to n
-	/// </summary>
-	DistPred(int n) {
-		distance.resize(n);
-		predecessor.resize(n);
-	}
-};
-
-/*!
-	\brief Construct the shortest path from start to end using the given predecessor and distance vectors.
-	
-	\param start ID of the starting point.
-	\param end ID of the end point. 
-	\param pred Predecessor matrix for the start node.
-	\param distances Distance matrix for pred
-	
-	\todo Replace exception with an assert statment. It shouldn't be triggered unless
-	there's a problem with this algorithm? 
-*/
-inline Path ConstructShortestPathFromPred(
-	int start, 
-	int end, 
-	const std::vector<size_t>& pred, 
-	const std::vector<float>& distances
-) {
-
-	// Create a new path and add the end point. 
-	Path p;
-	p.AddNode(end, 0);
-
-	// Return an empty path if there's no path from start to end
-	// indicated by the predecessor's value for the current node
-	// being the current node.
-	int current_node = end;
-	if (pred[current_node] == current_node) return Path{};
-
-	float last_cost = distances[current_node];
-
-	// Follow the predecessor matrix from end to start
-	while (current_node != start) {
-
-		// If this is triggered, there's something wrong with this algorithm
-		// because the path suddenly has more nodes than there are in the
-		// entire graph.
-		if (p.size() > pred.size())
-			throw std::exception("Path included more nodes than contaiend in the graph!");
-
-		// Get the next node from the predecessor matrix
-		int next_node = pred[current_node]; 
-		
-		// Get the current cost for traversing the graph so far.
-		float current_cost = distances[next_node];
-		
-		// Subtract the last cost from the graph to get the cost 
-		// of traversing this node
-		float de_facto_cost = last_cost - current_cost;
-
-		// Add this node to the list
-		p.AddNode(next_node, de_facto_cost);
-
-		// Save this node and cost as the last node/cost
-		last_cost = current_cost;
-		current_node = next_node;
-	}
-
-	// Flip the order of this since this algorithm generates it from end to start
-	p.Reverse();
-	return p;
-}
-
-/*! 
-	\brief Overload to call this with a distPred instead of the raw arrays. 
-
-	\param start Starting point of the path to generate.
-	\param end End point of the path to generate.
-	\param dist_pred DistPred containing the predecessor and distance matricies start.
-*/
-inline Path ConstructShortestPathFromPred(int start, int end, const DistPred& dist_pred) {
-	return ConstructShortestPathFromPred(start, end, dist_pred.predecessor, dist_pred.distance);
-}
-
-/*! 
-	\brief Build a row of the distance and predecessor matrices for the given node ID.
-
-	\param g the graph to build the predecessor and distance matrices for
-	\param id The ID of the node to generate the predecessor matrix from
-	
-	\returns 
-	A DistPred filled with the distance and predecessor arrays for ID in g.
-
-*/
-inline DistPred BuildDistanceAndPredecessor(const graph_t& g, int id) {
-	
-	// Construct an isntance of DistPred that can hold every node in the graph
-	int n = num_vertices(g);
-	DistPred dist_pred(n);
-
-	// Give boost a reference to the array in distpred
-	auto pm = boost::predecessor_map(&dist_pred.predecessor[0]);
-	vertex_descriptor start_vertex = vertex(id, g);
-
-	// Fill distpred's distance and predecssor arrays with info from the graph
-	dijkstra_shortest_paths_no_color_map(
-		g, 
-		start_vertex,
-		pm.distance_map(&dist_pred.distance[0]).weight_map(boost::get(&Edge_Cost::weight, g))
-	);
-
-	return dist_pred;
-}
-
 namespace HF::Pathfinding {
+	/// <summary>
+	/// Holds a distance and predecessor matrix for the boost pathfinding algorithm
+	/// </summary>
+	struct DistPred {
+		std::vector<float> distance;
+		std::vector<vertex_descriptor> predecessor;
+
+		DistPred() {};
+
+		/// <summary>
+		/// Resize both starting arrays  to n
+		/// </summary>
+		DistPred(int n) {
+			distance.resize(n);
+			predecessor.resize(n);
+		}
+	};
+
+	/*!
+		\brief Construct the shortest path from start to end using the given predecessor and distance vectors.
+
+		\param start ID of the starting point.
+		\param end ID of the end point.
+		\param pred Predecessor matrix for the start node.
+		\param distances Distance matrix for pred
+
+		\todo Replace exception with an assert statment. It shouldn't be triggered unless
+		there's a problem with this algorithm?
+	*/
+	inline Path ConstructShortestPathFromPred(
+		int start,
+		int end,
+		const std::vector<size_t>& pred,
+		const std::vector<float>& distances
+	) {
+
+		// Create a new path and add the end point. 
+		Path p;
+		p.AddNode(end, 0);
+
+		// Return an empty path if there's no path from start to end
+		// indicated by the predecessor's value for the current node
+		// being the current node.
+		int current_node = end;
+		if (pred[current_node] == current_node) return Path{};
+
+		float last_cost = distances[current_node];
+
+		// Follow the predecessor matrix from end to start
+		while (current_node != start) {
+
+			// If this is triggered, there's something wrong with this algorithm
+			// because the path suddenly has more nodes than there are in the
+			// entire graph.
+			if (p.size() > pred.size())
+				throw std::exception("Path included more nodes than contaiend in the graph!");
+
+			// Get the next node from the predecessor matrix
+			int next_node = pred[current_node];
+
+			// Get the current cost for traversing the graph so far.
+			float current_cost = distances[next_node];
+
+			// Subtract the last cost from the graph to get the cost 
+			// of traversing this node
+			float de_facto_cost = last_cost - current_cost;
+
+			// Add this node to the list
+			p.AddNode(next_node, de_facto_cost);
+
+			// Save this node and cost as the last node/cost
+			last_cost = current_cost;
+			current_node = next_node;
+		}
+
+		// Flip the order of this since this algorithm generates it from end to start
+		p.Reverse();
+		return p;
+	}
+
+	/*!
+		\brief Overload to call this with a distPred instead of the raw arrays.
+
+		\param start Starting point of the path to generate.
+		\param end End point of the path to generate.
+		\param dist_pred DistPred containing the predecessor and distance matricies start.
+	*/
+	inline Path ConstructShortestPathFromPred(int start, int end, const DistPred& dist_pred) {
+		return ConstructShortestPathFromPred(start, end, dist_pred.predecessor, dist_pred.distance);
+	}
+
+	/*!
+		\brief Build a row of the distance and predecessor matrices for the given node ID.
+
+		\param g the graph to build the predecessor and distance matrices for
+		\param id The ID of the node to generate the predecessor matrix from
+
+		\returns
+		A DistPred filled with the distance and predecessor arrays for ID in g.
+
+	*/
+	inline DistPred BuildDistanceAndPredecessor(const graph_t& g, int id) {
+
+		// Construct an isntance of DistPred that can hold every node in the graph
+		int n = num_vertices(g);
+		DistPred dist_pred(n);
+
+		// Give boost a reference to the array in distpred
+		auto pm = boost::predecessor_map(&dist_pred.predecessor[0]);
+		vertex_descriptor start_vertex = vertex(id, g);
+
+		// Fill distpred's distance and predecssor arrays with info from the graph
+		dijkstra_shortest_paths_no_color_map(
+			g,
+			start_vertex,
+			pm.distance_map(&dist_pred.distance[0]).weight_map(boost::get(&Edge_Cost::weight, g))
+		);
+
+		return dist_pred;
+	}
 
 	Path FindPath(BoostGraph* bg, int start_id, int end_id)
 	{
