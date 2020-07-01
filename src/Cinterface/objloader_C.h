ebfac49abd8f9c1728e075d21d9ead1382e0e571
Gemuele Aludino <g.aludino@gmail.com>
2020-07-01 18:31:38 -0400

    Example code for objloader_C provided in this commit.
    The example code within objloader_C.h has been tested in
    tests/src/OBJLoader.cpp.

--- src/Cinterface/objloader_C.h
+++ src/Cinterface/objloader_C.h
@@ -24,6 +24,32 @@ namespace HF {
 /// HF_STATUS::GENERIC_ERROR If the input was empty. HF_STATUS::INVALID_OBJ if the path didn't lead
 /// to a valid OBJ file, and HF_STATUS::NOT_FOUND if the file at the given path couldn't be found.
 /// </returns>
+
+/*!
+	\code
+		// Requires #include "objloader_C.h", #include "meshinfo.h", #include "objloader.h"
+
+		// Prepare parameters for LoadOBJ
+
+		// relative path begins where EXE file is located if file_path is not a path to a
+		// valid OBJ file, HF::Exceptions::FileNotFound is thrown
+		std::string file_path = "big_teapot.obj";
+
+		const int obj_length = file_path.size();
+
+		const float x_rot = 30;
+		const float y_rot = 20;
+		const float z_rot = 55;
+
+		std::vector<HF::Geometry::MeshInfo>* info = nullptr;
+
+		// Call LoadOBJ
+		LoadOBJ(file_path.c_str(), obj_length, x_rot, y_rot, z_rot, &info);
+
+		// Release memory for info once finished with it
+		DestroyMeshInfo(info);
+	\endcode
+*/
 C_INTERFACE LoadOBJ(
 	const char* obj_path,
 	int length,
@@ -40,7 +66,37 @@ C_INTERFACE LoadOBJ(
 /// <param name="num_indices">Length of the indices array.</param>
 /// <param name="vertices"> Vertices of the mesh. Each 3 floats represent the X,Y, and Z of a point in space.</param>
 /// <param name="num_vertices">Length of the vertices array.</param>
+/// <param name="name">Desired name for the mesh</param>
+/// <param name="id">Desired ID for the mesh</param>
 /// <returns>HF_STATUS::OK if the mesh was loaded successfully. HF_STATUS::INVALID_OBJ if the given indices and vertices didn't create a valid mesh. </returns>
+
+/*!
+	\code
+		// Requires #include "objloader_C.h", #include "meshinfo.h"
+
+		// Prepare parameters for StoreMesh
+		std::vector<HF::Geometry::MeshInfo>* info = nullptr;
+
+		int mesh_indices[] = { 0, 1, 2 };
+		const int mesh_num_indices = sizeof * mesh_indices;
+		float mesh_vertices[] = { 34.1, 63.9, 16.5, 23.5, 85.7, 45.2, 12.0, 24.6, 99.4 };
+		const int mesh_num_vertices = sizeof * mesh_vertices;
+
+		std::string mesh_name = "This mesh";
+		const int mesh_id = 0;
+
+		// Call StoreMesh
+		if (StoreMesh(&info, mesh_indices, mesh_num_indices, mesh_vertices, mesh_num_vertices, mesh_name.c_str(), mesh_id)) {
+			std::cout << "StoreMesh successful" << std::endl;
+		}
+		else {
+			std::cout << "StoreMesh unsuccessful" << std::endl;
+		}
+
+		// Release memory for info once finished with it
+		DestroyMeshInfo(info);
+	\endcode
+*/
 C_INTERFACE StoreMesh(
 	std::vector<HF::Geometry::MeshInfo>** out_info,
 	const int* indices,
@@ -57,6 +113,47 @@ C_INTERFACE StoreMesh(
 /// <param name="xrot"> Rotation around the x axis in degrees.</param>
 /// <param name="yrot"> Rotation around the y axis in degrees.</param>
 /// <param name="zrot"> Rotation around the z axis in degrees.</param>
+
+/*!
+	\code
+		// Requires #include "objloader_C.h", #include "meshinfo.h"
+
+		// Prepare parameters for StoreMesh
+		std::vector<HF::Geometry::MeshInfo>* info = nullptr;
+
+		int mesh_indices[] = { 0, 1, 2 };
+		const int mesh_num_indices = sizeof * mesh_indices;
+		float mesh_vertices[] = { 34.1, 63.9, 16.5, 23.5, 85.7, 45.2, 12.0, 24.6, 99.4 };
+		const int mesh_num_vertices = sizeof * mesh_vertices;
+
+		std::string mesh_name = "This mesh";
+		const int mesh_id = 0;
+
+		// Call StoreMesh
+		if (StoreMesh(&info, mesh_indices, mesh_num_indices, mesh_vertices, mesh_num_vertices, mesh_name.c_str(), mesh_id)) {
+			std::cout << "StoreMesh successful" << std::endl;
+		}
+		else {
+			std::cout << "StoreMesh unsuccessful" << std::endl;
+		}
+
+		// Prepare desired rotation values
+		const float x_rot = 10;
+		const float y_rot = 10;
+		const float z_rot = 20;
+
+		// Call RotateMesh
+		if (RotateMesh(info, x_rot, y_rot, z_rot)) {
+			std::cout << "RotateMesh successful" << std::endl;
+		}
+		else {
+			std::cout << "RotateMesh unsuccessful" << std::endl;
+		}
+
+		// Release memory for info once finished with it
+		DestroyMeshInfo(info);
+	\endcode
+*/
 C_INTERFACE RotateMesh(
 	std::vector<HF::Geometry::MeshInfo>* mesh_to_rotate,
 	float xrot,
@@ -65,11 +162,41 @@ C_INTERFACE RotateMesh(
 );
 
 
-
 /// <summary>
-/// Delete all instaces of mesh info in the given vector
+/// Delete all instances of mesh info in the given vector
 /// </summary>
 /// <param name="mesh_to_destroy"> The mesh info to destroy. </param>
+
+/*!
+	\code
+		// Requires #include "objloader_C.h", #include "meshinfo.h"
+
+		// Prepare parameters for LoadOBJ
+
+		// relative path begins where EXE file is located if file_path is not a path to a
+		// valid OBJ file, HF::Exceptions::FileNotFound is thrown
+		std::string file_path = "big_teapot.obj";
+
+		const int obj_length = file_path.size();
+
+		const float x_rot = 30;
+		const float y_rot = 20;
+		const float z_rot = 55;
+
+		std::vector<HF::Geometry::MeshInfo>* info = nullptr;
+
+		// Call LoadOBJ
+		if (LoadOBJ(file_path.c_str(), obj_length, x_rot, y_rot, z_rot, &info)) {
+			std::cout << "LoadOBJ successful" << std::endl;
+		}
+		else {
+			std::cout << "LoadOBJ unsuccessful" << std::endl;
+		}
+
+		// Release memory for info once finished with it
+		DestroyMeshInfo(info);
+	\endcode
+*/
 C_INTERFACE DestroyMeshInfo(std::vector<HF::Geometry::MeshInfo>* mesh_to_destroy);
 
 /**@}*/
