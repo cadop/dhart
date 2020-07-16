84a0e0e1525e4ae159d2d2b63157d391091ea80a
db368 <db368@njit.edu>
2020-07-16 16:27:40 -0400

    Geometry namespace documentation

--- src/Csharp/packages/geometry/src/CommonRotations.cs
+++ src/Csharp/packages/geometry/src/CommonRotations.cs
@@ -2,18 +2,17 @@ using System.Numerics;
 
 namespace HumanFactors.Geometry
 {
-    /// <summary>
-    /// Contains several constants for frequently used rotations.
-    /// </summary>
+    /*! \brief A collection of rotations that are frequently useful in HumanFactors. */
     public static class CommonRotations
     {
-        /// <summary>
-        /// To convert a mesh from Z-Up to Y-Up
-        /// </summary>
+        /*! \brief To rotate a mesh from Z-Up to Y-Up */
         public static Vector3D Zup_To_Yup = new Vector3D(-90, 0,  0);
-        /// <summary>
-        /// To convert a mesh from Y-Up to Z-Up.
-        /// </summary>
+        /*! 
+            \brief Will rotate a mesh from Y-Up to Z-Up.
+            \remarks 
+            Useful for rotating Y-Up meshes so the GraphGenerator
+            can run on them.
+        */
         public static Vector3D Yup_To_Zup = new Vector3D(90, 0,  0);
     }
 }
