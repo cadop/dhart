c7f4f411ea61fe87b42ea10e906d9286f4e1306b
db368 <db368@njit.edu>
2020-07-17 10:30:58 -0400

    Raytracer documentation

--- src/Csharp/packages/raytracer/src/EmbreeBVH.cs
+++ src/Csharp/packages/raytracer/src/EmbreeBVH.cs
@@ -5,15 +5,41 @@ using System.Runtime.Remoting.Messaging;
 
 namespace HumanFactors.RayTracing
 {
-    /// <summary>
-    /// Contains A BVH for use with the <see cref="EmbreeRaytracer"/>.
-    /// </summary>
+    /*!
+        \brief A Bounding Volume Hierarchy for the \link EmbreeRayTracer \endlink.
+            
+        \details
+        Upon calling the constructor for this class, a Bounding Volume Hierarchy is generated 
+        for the \link EmbreeRaytracer \endlink. Without this structure, the raytracer cannot be used.
+
+        \remarks
+        A more detailed description of Embree's BVH is available in intel's paper:
+        Embree: A Kernel Framework for Efficient CPU Ray Tracing
+        http://cseweb.ucsd.edu/~ravir/274/15/papers/a143-wald.pdf.
+
+        \internal
+            \note
+            In C++ this is is an EmbreeRayTracer.
+
+            \todo
+            Functions to add meshes to an existing BVH
+        \endinternal
+
+        \see EmbreeRaytracer for functions to use this BVH for ray intersections. 
+    */
     public class EmbreeBVH : HumanFactors.NativeUtils.NativeObject
     {
-        /// <summary>
-        /// Create a new BVH for the mesh located at <paramref name="MI"/>
-        /// </summary>
-        /// <param name="MI">Mesh to use for BVH construction</param>
+        /*!
+            \brief Generate a BVH for an instance of MeshInfo. 
+            
+            \param MI Mesh to generate the BVH from. 
+
+            \exception Exception Embree failed to create a BVH for the given mesh.
+
+            \internal
+                \todo Add missing dependency exception
+            \endinternal
+       */
         public EmbreeBVH(MeshInfo MI) : base(NativeMethods.C_ConstructRaytracer(MI.DangerousGetHandle()), MI.pressure) { }
 
         protected override bool ReleaseHandle()
