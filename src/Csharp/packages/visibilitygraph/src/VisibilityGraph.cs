088f7ec18a563aee067eda7fa7b44d228767122e
db368 <db368@njit.edu>
2020-07-17 13:23:21 -0400

    Visibility Graph Documentation

--- src/Csharp/packages/visibilitygraph/src/VisibilityGraph.cs
+++ src/Csharp/packages/visibilitygraph/src/VisibilityGraph.cs
@@ -10,35 +10,59 @@ using System.Dynamic;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
+/*!
+    \brief Evaluate visibility between a set of points. 
+
+    \details
+    A Visibility Graph is a graph of points of space locations that have a
+    clear line of sight to each other. Edges in a Visibility Graph are bidirectional,
+    since if one node can see another node, the inverse is true. Generating a Visibility
+    Graph from a set of nodes consists of performing a line of sight check between each
+    combination of nodes, and creating an edge if the line of sight check passes. In this
+    implementation, the cost of every edge in the visibility graph is equal to the distance
+    between the parent and child nodes. 
+
+    \see SpatialStructures.Graph for more information about the type returned from this function.
+    \see RayTracing.EmbreeBVH for info on the BVH type required to call these functions. 
+
+*/
 namespace HumanFactors.VisibilityGraph
 {
-    /// <summary> Contains all methods for generating Visibility Graphs. </summary>
+    /// <summary> Contains all methods for generating Visibility Graphs.
     public static class VisibilityGraph
     {
-        /// <summary>
-        /// Create a visibility graph from every node to every node in <paramref name="nodes" />.
-        /// </summary>
-        /// <param name="bvh"> The BVH to intersect with. </param>
-        /// <param name="nodes"> The nodes to use in the visibility graph. </param>
-        /// <param name="height">
-        /// The height to offset each node from the ground. Any nodes that clip into geometry when
-        /// offset by height will not be considered for any edge connections.
-        /// </param>
-        /// <param name="directed">
-        /// if set to <c> true </c> generate a directed graph, otherwise generate an undirected graph.
-        /// </param>
-        /// <returns> A Graph containing the visbility graph. </returns>
-        /// <remarks>
-        /// <para>
-        /// It is suggested in most contexts to set <paramref name="directed" /> to <see
-        /// langword="true" />, due to the complexity of generating a visibility graph. An
-        /// undirected visibility graph performs N^2 operations, while a directed visibility graph
-        /// only performs n(n+1)/2 operations. The Directed graph saves space by only storing
-        /// connections between two nodes in the edgelist of a one of the nodes instead of both nodes.
-        /// </para>
-        /// The results of the visibility graph can be easily summarized per node by using the
-        /// Graph's <see cref="Graph.AggregateEdgeCosts(GraphEdgeAggregation, bool)" /> method.
-        /// </remarks>
+        /*!
+            \brief Create a visibility graph from every node to every node in nodes.
+
+            \param bvh The BVH to intersect with.
+            \paam nodes The nodes to use in the visibility graph.
+            \param height The height to offset each node from the ground. 
+            \param directed  if set to true generate a directed graph, otherwise generate an undirected graph.
+
+            \returns
+            A Graph where each node is a node in nodes, and each edge represents a clear line of sight between the parent and
+            child node. Any nodes that clip into geometry when offset by height will not be considered 
+            for any edge connections and will always have no outgoing or incoming edges.
+            
+            \attention
+            When directed is true. Edges between nodes will only be stored in the edges of the node with the 
+            lower ID. To store the edge in the edge array of both nodes use false but be warned, it will
+            take longer to execute and take up more memory. Read the following section for more info.
+
+            \par Directed vs Undirected 
+            For the sake of this function, an undirected visibility graph describes a graph
+            where edge is only stored in the graph for the node with the lowest ID. For example, 
+            The first node will check for edges from itself to every node in the set, node 2 will
+            check for edges from itself to every other node except for the first node, node 3
+            will check every node except for the first and second. In contrast, a directed graph
+            will check every node against every other node in the graph regardless of ID. In most cases
+            you will want to use the undirected algorithm, as it only performs  n(n+1)/2 operations vs
+            the directed's n^2 operations.
+			
+            \warning Graphs output by this function CANNOT be modified.
+
+            \see SpatialStructures.Graph.AggregateEdgeCosts for a way to easily summarize the results of a visibility graph.
+       */
         public static Graph GenerateAllToAll(EmbreeBVH bvh, IEnumerable<Vector3D> nodes, float height = 1.7f, bool directed = true)
         {
             IntPtr potential_graph = NativeMethods.C_AllToAllVisibilityGraph(bvh.Pointer, nodes, height, directed);
@@ -46,22 +70,29 @@ namespace HumanFactors.VisibilityGraph
             else return new Graph(potential_graph);
         }
 
-        /// <summary>
-        /// Generate a directed visibility graph from nodes in <paramref name="group_a" /> to the
-        /// nodes in <paramref name="group_b" />.
-        /// </summary>
-        /// <param name="bvh"> The geometry to use for generating the visibility graph. </param>
-        /// <param name="group_a"> The nodes to fire rays from. </param>
-        /// <param name="group_b"> The nodes to fire rays to. </param>
-        /// <param name="height">
-        /// The height to offset nodes from the ground. Any nodes that clip into geometry when
-        /// offset by height will not be considered for any edge connections.
-        /// </param>
-        /// <returns>
-        /// A graph contanining the visibility graph. Nodes in group_a will be assigned IDs before
-        /// those in group_b. <seealso cref="VisibilityGraph.GenerateAllToAll(EmbreeBVH,
-        /// IEnumerable{Vector3D}, float, bool)" />
-        /// </returns>
+        /*!
+            \brief Generate a directed visibility graph from nodes in `group_a` to the nodes in `group_b.`
+
+            \param bvh The geometry to use for generating the visibility graph.
+            \param group_a The nodes to cast rays from.
+            \param group_b The nodes to cast rays to.
+            \param height The height to offset nodes from the ground. 
+            
+            \returns
+            A graph contanining the visibility graph. Since a new graph is created, all nodes will
+            be assigned ids for access within it. First every node in group_a will be assigned
+            ids in order, then all nodes in `group_b` will be assigned ids in order. Nodes in `group_a` will
+            be assigned IDs before those in `group_b`. All edges will be from nodes in `group_a` to nodes in
+            `group_b`. Any nodes that clip into geometry when offset by height will not be considered for
+            any edge connections and will always have no outgoing or incoming edges.
+            
+            \pre No nodes in `group_a` also exist in `group_b` and vice versa.
+
+            \attention
+            Any nodes that clip into geometry when offset by `height` will not be considered for any edge connections.
+            
+            \see GenerateAllToAll to generate a graph from every node to every other node. 
+        */
         public static Graph GenerateGroupToGroup(
             EmbreeBVH bvh,
             IEnumerable<Vector3D> group_a,
