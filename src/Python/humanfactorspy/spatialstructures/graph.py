23ef58e1c2578c7726bcc87f0824e5996c87f89d
cadop <cadop@njit.edu>
2020-07-30 12:17:23 -0400

    add method to get node positions as regular numpy array

--- src/Python/humanfactorspy/spatialstructures/graph.py
+++ src/Python/humanfactorspy/spatialstructures/graph.py
@@ -2,6 +2,7 @@ import ctypes
 import numpy
 from numpy.lib import recfunctions as rfn
 from scipy.sparse import csr_matrix
+from scipy.spatial.distance import cdist
 from ctypes import c_float, c_int, c_void_p
 from typing import *
 from enum import IntEnum
@@ -128,6 +129,7 @@ class Graph:
             child: Where the edge from parent is going to. Must be the same type of parent
                 i.e. if parent is an int, then the child must also be an int
             cost: the cost from parent to child
+            
         """
 
         # convert string to bytes
@@ -154,7 +156,112 @@ class Graph:
         ret = spatial_structures_native_functions.GetNodesFromGraph(self.graph_ptr)
         return NodeList(ret[0], ret[1])
 
-    def get_node_points(self):
+    def get_closest_nodes(self, p_desired, x=True, y=True, z=True):
+        """ Get the closet nodes to the input set of points
+
+        Parameters
+        ----------
+
+        p_desired : ndarray
+            n x m shape where n is the number of points an m is equal to the axis set to True
+        x : bool, optional
+            Include x axis in the distance comparison, Default True
+        y : bool, optional
+            Include y axis in the distance comparison, Default True
+        z : bool, optional
+            Include z axis in the distance comparison, Default True
+
+        Returns
+        -------
+
+        int or ndarray[int]
+            int if a single point is passed, ndarray if multiple points are passed,
+            where ndarray is size of number of points passed in p_desired
+
+        Warning
+        -------
+
+        Size of points in `p_desired` must be same as the number of x,y,z axis set to True 
+
+        Note
+        ----
+
+        In future this should be calling a method in C interface instead of implemented in Python
+
+        Examples
+        --------
+
+        >>> import matplotlib.pyplot as plt
+        >>> import numpy as np
+        >>> 
+        >>> import humanfactorspy
+        >>> from humanfactorspy.geometry import LoadOBJ, CommonRotations
+        >>> from humanfactorspy.graphgenerator import GenerateGraph
+        >>> from humanfactorspy.raytracer import EmbreeBVH
+        >>> from humanfactorspy.pathfinding import DijkstraShortestPath
+        >>> 
+        >>> # Load BVH
+        >>> obj_path = humanfactorspy.get_sample_model("energy_blob_zup.obj")
+        >>> loaded_obj = LoadOBJ(obj_path)
+        >>> embree_bvh = EmbreeBVH(loaded_obj)
+        >>> 
+        >>> # Set graph parameters 
+        >>> start_point, spacing, max_nodes = (-30, 0, 20), (1, 1, 10), 100000
+        >>> 
+        >>> # Generate the graph
+        >>> graph = GenerateGraph(embree_bvh, start_point, spacing, max_nodes)
+        
+        Check closest nodes using single x,y point
+
+        >>> closest_node = graph.get_closest_nodes(np.array([30,0]), z=False)
+        >>> print("Closest Node: ", closest_node)
+        Closest Node:  1795
+
+        Check closest nodes using multiple x,y points
+
+        >>> closest_node = graph.get_closest_nodes(np.array([[30,0],[20,20]]), z=False)
+        >>> print("Closest Node: ", closest_node)
+        Closest Node:  [1795 2513]
+
+        Check closest nodes using single x,y,z point
+
+        >>> closest_node = graph.get_closest_nodes(np.array([30,0,0]))
+        >>> print("Closest Node: ", closest_node)
+        Closest Node:  1796
+
+        Check closest nodes using multiple x,y,z points
+
+        >>> closest_node = graph.get_closest_nodes(np.array([[30,0,0],[20,20,0]]))
+        >>> print("Closest Node: ", closest_node)
+        Closest Node:  [1796 2513]
+
+        """
+
+        # if this is one point, wrap in a list
+        if len(p_desired.shape) == 1:
+            p_desired=[p_desired]
+            # Get the closest node to the point 
+            closest_nodes = cdist(p_desired, self.get_node_points(x=x, y=y, z=z)).argmin()
+
+        else:
+            # Get the closest node to the point and return array
+            closest_nodes = cdist(p_desired, self.get_node_points(x=x, y=y, z=z)).argmin(axis=1)
+
+        return closest_nodes
+
+    def get_closest_points():
+        """ Get the closest point in the graph to the input set of points
+
+        Todo
+        ----
+
+        Implement this
+
+        """
+
+        return 
+
+    def get_node_points(self, x=True, y=True, z=True):
         """
         Get the nodes of the graph as xyz values in a numpy array by defining the array name
         view to be used as the x,y,z values. It uses the `structured_to_unstructured` 
@@ -163,13 +270,20 @@ class Graph:
         Parameters
         ----------
 
-        None
+        x : bool, optional
+        y : bool, optional
+        z : bool, optional
 
         Returns
         -------
 
         ndarray[n x 3]
             where n is the number of nodes in the graph and 3 is an x,y,z point
+
+        Notes
+        -----
+
+        Copys? (TODO check this) which means the array will not change if the graph is modified
 
         Examples
         --------
@@ -181,10 +295,17 @@ class Graph:
 
         # first get the nodes
         nodes = self.getNodes()
-        # extract the array names of the xyz values
-        node_xyz = nodes.array[['x','y','z']]
+
+        # Create list of point names to return, e.g, ['x','y','z']
+        point_names = []
+        if x: point_names.append('x')
+        if y: point_names.append('y')
+        if z: point_names.append('z')
+
+        # extract the array names of the xyz values of interest
+        node_xyz = nodes.array[point_names]
         # convert to an unstructured array to be seen like normal numpy array
-        xyz_array = rfn.structured_to_unstructured(node_xyz)
+        node_point_array = rfn.structured_to_unstructured(node_xyz)
 
         return node_point_array
 
