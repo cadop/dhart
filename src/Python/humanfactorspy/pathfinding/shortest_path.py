a2626e9a3af76c63615b74ec35e401b38765261f
db368 <db368@njit.edu>
2020-07-31 10:40:18 -0400

    Add a doctest for shortest path demonstrating alternate costs

--- src/Python/humanfactorspy/pathfinding/shortest_path.py
+++ src/Python/humanfactorspy/pathfinding/shortest_path.py
@@ -129,6 +129,59 @@ def DijkstraShortestPath(
         [(10., 1) ( 0., 3)]
 
 
+    Example:
+       Creating a graph then calculating an alternate cost type
+       and generating a path on it.
+        
+       >>> from humanfactorspy import get_sample_model
+       >>> from humanfactorspy.geometry.obj_loader import LoadOBJ
+       >>> from humanfactorspy.graphgenerator import GenerateGraph
+       >>> from humanfactorspy.pathfinding import DijkstraShortestPath
+       >>> from humanfactorspy.raytracer import EmbreeBVH
+       >>> from humanfactorspy.spatialstructures.cost_algorithms import (
+       ...    CalculateEnergyExpenditure, CostAlgorithmKeys)
+
+       >>> # Load the energy blob and create a BVH from it
+       >>> energy_blob_path = get_sample_model("energy_blob_zup.obj")
+       >>> energy_blob_mesh = LoadOBJ(energy_blob_path)
+       >>> bvh = EmbreeBVH(energy_blob_mesh)
+
+       >>> # Define graph generator parameters
+       >>> start_point = (-30, 0, 20)
+       >>> spacing = (1, 1, 10)
+       >>> max_nodes = 10000
+       >>> up_step, down_step = 5, 5
+       >>> up_slope, down_slope = 60, 60
+       >>> max_step_connections = 1
+
+       >>> # Generate a graph on it
+       >>> g = GenerateGraph(bvh, start_point, spacing, max_nodes,
+       ...                 up_step, up_slope, down_step, down_slope,
+       ...                 max_step_connections, cores=-1)
+
+       >>> # Compress the graph
+       >>> csr = g.CompressToCSR()
+
+       >>> # Generate an alternate cost type and store it in the graph
+       >>> CalculateEnergyExpenditure(g)
+
+       >>> # Generate a path using the energy expenditure cost and 
+       >>> # distance (the default)
+       >>> start_point = 1
+       >>> end_point = 150
+       >>> energy_expend_key = CostAlgorithmKeys.ENERGY_EXPENDITURE
+       >>> distance_path = DijkstraShortestPath(g, start_point, end_point)
+       >>> energy_path = DijkstraShortestPath(
+       ...     g, start_point, end_point, energy_expend_key)
+       
+       >>> # Print both paths
+       >>> print("Distance Path:", distance_path.array)
+       Distance Path: [(1.000002 ,   1) (0.9999999,  12) (1.       ,  26) (1.0000005,  43)
+        (1.0000019,  64) (1.0000081,  89) (1.4148979, 118) (0.       , 150)]
+       
+       >>> print("Energy Path:", energy_path.array)
+       Energy Path: [(2.4610138,   1) (2.5      ,  12) (2.5      ,  26) (2.4804544,  43)
+        (2.461011 ,  64) (2.4224558,  89) (2.7486944, 118) (0.       , 150)]
     """
 
     # Compress the graph if it isn't already.
