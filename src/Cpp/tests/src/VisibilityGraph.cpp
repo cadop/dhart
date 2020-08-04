00917fed821da3124c632cd17d32d2503fa93cb9
db368 <db368@njit.edu>
2020-08-04 09:22:20 -0400

    Add two tests to prove correctness of GroupToGroup visibility graph

--- src/Cpp/tests/src/VisibilityGraph.cpp
+++ src/Cpp/tests/src/VisibilityGraph.cpp
@@ -10,6 +10,7 @@
 #include <visibility_graph.h>
 #include <string>
 #include <array>
+#include <visibility_graph_C.h>
 
 using namespace HF::VisibilityGraph;
 using HF::SpatialStructures::Graph;
@@ -17,11 +18,16 @@ using HF::SpatialStructures::Node;
 using HF::RayTracer::EmbreeRayTracer;
 using HF::Geometry::MeshInfo;
 using namespace HF::Geometry;
+using std::vector;
 
 const std::string plane_path = "plane.obj"; // A flat plane
 
 // A flat plane with a wall perpendicular to the y axis 
 const std::string walled_plane_path = "plane_walled.obj"; 
+
+EmbreeRayTracer CreatePlaneTracer() {
+	return EmbreeRayTracer(LoadMeshObjects(plane_path, HF::Geometry::ONLY_FILE, true));
+}
 
 TEST(_VisibilityGraph, NodesOnFlatPlaneAllConnect) {
 	EmbreeRayTracer plane_tracer(LoadMeshObjects(plane_path, HF::Geometry::ONLY_FILE, true));
@@ -71,6 +77,102 @@ TEST(_VisibilityGraph, NodesOnFlatPlaneWithWallDontConnect) {
 			continue;
 		}
 
+	}
+}
+
+
+// This is testing whether or not the group to group algorithm
+// Produces a valid graph
+TEST(_VisibilityGraph, GroupConstructsValidGraph) {
+	auto raytracer = CreatePlaneTracer();
+	
+	vector<Node> nodes = {
+		{0,0,1},
+		{1,1,1},
+		{1,0,1},
+		{0,0,-1},
+	};
+
+	vector<Node> Group1 = { nodes[0], nodes[1] };
+	vector<Node> Group2 = { nodes[2], nodes[3] };
+
+	auto graph = GroupToGroup(raytracer, Group1, Group2);
+
+	// Ensure that the graph's size equals the number of nodes that were created for it.
+	ASSERT_EQ(4, graph.size());
+	auto sums = graph.AggregateGraph(HF::SpatialStructures::COST_AGGREGATE::COUNT);
+	
+	// Assert that these values meet the expectation
+	ASSERT_EQ(1, sums[0]);
+	ASSERT_EQ(1, sums[1]); // both of these should only have a connection to node 3
+	ASSERT_EQ(0, sums[2]);
+	ASSERT_EQ(0, sums[3]);
+
+	for (const auto& node : nodes) {
+		std::cerr << node << std::endl;
+	}
+
+}
+
+// Tests if the above holds for the C_INterface
+TEST(C_VisibilityGraph, GroupConstructsValidGraph) {
+	auto raytracer = CreatePlaneTracer();
+
+	// Defin enodes
+	vector<Node> nodes = {
+		{0,0,1},
+		{1,1,1},
+		{1,0,1},
+		{0,0,-1},
+	};
+
+	// Put each group into a seperate vector of floats. These will be
+	// Passed to the C_Interface
+	vector<float> group_a = {
+		nodes[0].x, nodes[0].y, nodes[0].z,
+		nodes[1].x, nodes[1].y, nodes[1].z,
+	};
+
+	vector<float> group_b = {
+		nodes[2].x, nodes[2].y, nodes[2].z,
+		nodes[3].x, nodes[3].y, nodes[3].z,
+	};
+
+	// Define a graph to use as an output parameter
+	Graph* G;
+
+	// Call the C_Interface Visibility graph
+	auto res = CreateVisibilityGraphGroupToGroup(
+		&raytracer,
+		group_a.data(),
+		group_a.size()/3,
+		group_b.data(),
+		group_b.size()/3,
+		&G,
+		1.7
+	);
+	
+	// Create a reference to G for easier use
+	const auto & out_graph = *G;
+
+	// Assert that it is of the correct size
+	ASSERT_EQ(4, out_graph.size());
+
+	// Assert that the connections are correct
+	auto sums = out_graph.AggregateGraph(HF::SpatialStructures::COST_AGGREGATE::SUM);
+	ASSERT_EQ(1, sums[0]);
+	ASSERT_EQ(1, sums[1]); // both of these should only have a connection to node 3
+	ASSERT_EQ(0, sums[2]);
+	ASSERT_EQ(0, sums[3]);
+
+	// Assert that all nodes are finite to prove it isn't reading outside of the array's bounds
+	for (const auto& node : out_graph.Nodes()) {
+		ASSERT_TRUE(
+			std::isfinite(node.x),
+			std::isfinite(node.y),
+			std::isfinite(node.z),
+		);
+		std::cerr << node << std::endl;
 	}
 }
 
