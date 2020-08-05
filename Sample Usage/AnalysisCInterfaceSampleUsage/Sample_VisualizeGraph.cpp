59e9f857f97b193cb973b5addf7f852bbf2c6d47
Gemuele Aludino <g.aludino@gmail.com>
2020-08-05 14:33:46 -0400

    Removed commented Python code, left TODO comment for 'Plot the nodes in x/y plane'
    Will have to further research how to properly do this.

--- Sample Usage/AnalysisCInterfaceSampleUsage/Sample_VisualizeGraph.cpp
+++ Sample Usage/AnalysisCInterfaceSampleUsage/Sample_VisualizeGraph.cpp
@@ -462,71 +462,10 @@ void CInterfaceTests::visualize_graph(HINSTANCE dll_hf) {
 		*(it_z++) = n.z;
 	}
 
+	//
+	// TODO: Plot the graph of the nodes in the x/y plane
+	//
 	std::cout << "TODO: output image of graph nodes in x/y plane" << std::endl;
-
-	//
-	// Plot the graph in 3D
-	//
-	// The graph can be plotted in 3D for illustrative purposes
-	// (although it is better to load an obj or use a proper engine like OpenGL.)
-	//
-	/*
-		# Plot the graph in 3D
-		fig = plt.figure()
-		ax = Axes3D(fig)
-		ax.view_init(azim=-123, elev=15)
-
-		ax.scatter(x, y, z)
-		plt.show()
-	*/
-	//
-	// TODO C++, may just do a 2D ASCII-art example instead?
-	//
-
-
-	//
-	// From the top view, this can be plotted in x, y and color nodes by the z value
-	//
-	/*
-		# Plot the graph
-		plt.scatter(x, y, c=z, alpha=0.5)
-		plt.show()
-	*/
-	//
-	// TODO C++, may just do a 2D ASCII-art example instead?
-	//
-
-	//
-	// To illustrate the thresholds of this wavy hill, we can reduce the slope tolerance
-	// and plot a new graph which shows certain areas are not reachable.
-	//
-	/*
-		# Set a smaller threshold for slope
-		up_slope, down_slope = 5, 5
-
-		# Generate the Graph
-		graph = GenerateGraph(bvh, start_point, spacing, max_nodes,
-								up_step,up_slope,down_step,down_slope,
-								max_step_connections, cores=-1)
-
-		# Convert the graph to a CSR
-		csr_graph = graph.CompressToCSR()
-
-		# Get the nodes of the graph as a list of x,y,z,type,id tuples
-		nodes = graph.getNodes()
-
-		# get the x,y,z coordinates of the nodes and set the color to the z value
-		x = [ n[0] for n in nodes ] # x coordinate
-		y = [ n[1] for n in nodes ] # y coordinate
-		z = [ n[2] for n in nodes ] # z coordinate
-
-		# Plot the graph
-		plt.scatter(x, y, c=z, alpha=0.5)
-		plt.show()
-	*/
-	//
-	// TODO C++, may just do a 2D ASCII-art example instead?
-	//
 
 	///
 	/// Memory resource cleanup.
