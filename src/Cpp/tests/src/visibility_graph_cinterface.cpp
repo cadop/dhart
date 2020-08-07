677e4b9aa96ffb9b9c406eb075e42f455b111e00
Gemuele Aludino <g.aludino@gmail.com>
2020-08-07 18:09:04 -0400

    Addition of visibility_graph_cinterface.cpp for testing/snippets. CMakeLists.txt modified to accommodate this file.

---
+++ src/Cpp/tests/src/visibility_graph_cinterface.cpp
@@ -0,0 +1,24 @@
+/*!
+	\file		visibility_graph_cinterface.cpp
+	\brief		Unit test source file for testing the functionality of visibility_graph_C
+
+	\author		Gem Aludino
+	\date		07 Aug 2020
+*/
+
+#include "gtest/gtest.h"
+#include "visibility_graph_C.h"
+
+namespace CInterfaceTests {
+	TEST(_visibility_graph_cinterface, CreateVisibilityGraphAllToAll) {
+
+	}
+
+	TEST(_visibility_graph_cinterface, CreateVisibilityGraphAllToAllUndirected) {
+
+	}
+
+	TEST(_visibility_graph_cinterface, CreateVisibilityGraphGroupToGroup) {
+
+	}
+}
