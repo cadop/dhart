1f76ef70ab25bd5657be5c6bfdfd6372129c9e6f
Gemuele Aludino <g.aludino@gmail.com>
2020-08-19 10:30:43 -0400

    Added spatialstructures_C_cinterface.cpp, edited CMakeLists.txt
    Adding spatialstructures_C_cinterface.cpp for testing and storing snippets. CMakeLists.txt was modified to accommodate this new file.

---
+++ src/Cpp/tests/src/spatialstructures_C_cinterface.cpp
@@ -0,0 +1,113 @@
+/*!
+	\file		spatialstructures_C_cinterface.cpp
+	\brief		Unit test source file for functions tested from spatialstructures_C.h
+
+	\author		TBA
+	\date		19 Aug 2020
+*/
+
+#include "gtest/gtest.h"
+
+#include "graph.h"
+#include "constants.h"
+#include "HFExceptions.h"
+
+#include "spatialstructures_C.h"
+
+namespace CInterfaceTests {
+	TEST(_spatialstructures_cinterface, AlgorithmCostTitle) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetAllNodesFromGraph) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetEdgesForNode) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetSizeOfNodeVector) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetSizeOfEdgeVector) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetEdgeCost) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, AggregateCosts) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, CreateGraph) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, AddEdgeFromNodes) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, AddEdgeFromNodeIDs) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetCSRPointers) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetNodeID) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, Compress) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, ClearGraph) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, DestroyNodes) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, DestroyEdges) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, DestroyGraph) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, CalculateAndStoreCrossSlope) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, CalculateAndStoreEnergyExpenditure) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, AddNodeAttributes) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetNodeAttributes) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, DeleteScoreArray) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, ClearAttributeType) {
+
+	}
+
+	TEST(_spatialstructures_cinterface, GetSizeOfGraph) {
+
+	}
+}
