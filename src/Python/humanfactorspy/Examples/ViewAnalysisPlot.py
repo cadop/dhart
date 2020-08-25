b4147bab7d930d22cad469235a07ab8f4077e4a7
cadop <cadop@njit.edu>
2020-08-25 08:48:17 -0400

    Update ViewAnalysisPlot.py

--- src/Python/humanfactorspy/Examples/ViewAnalysisPlot.py
+++ src/Python/humanfactorspy/Examples/ViewAnalysisPlot.py
@@ -90,10 +90,10 @@ for i in range(5):
     z = [ n[2] for n in nodes ]
 
     height = 1.7 # Set a height offset to cast rays from the points
-    points = graph.get_node_points() # Define points as the graph nodes
-    VG = VisibilityGraphAllToAll(bvh, points, height) # Calculate the visibility graph
-    visibility_graph = VG.CompressToCSR() # Convert to a CSR (matrix)
-    scores = VG.AggregateEdgeCosts(2, True)
+    ray_count = 100
+    scores = SphericalViewAnalysisAggregate(bvh, nodes, ray_count, height, 
+                                            upward_fov = 20, downward_fov=20, 
+                                            agg_type=AggregationType.AVERAGE)
 
     # Plot the graph using visibility graph as the colors
     fig = plt.figure(figsize=(6,6))
