5aa6b5e498105e592cf8a72f024e8208ebf2334b
db368 <db368@njit.edu>
2020-08-25 16:23:29 -0400

    Copy over dlls/examplemodels on build

--- src/Csharp/Examples/StringsToCosts/strtoc.cs
+++ src/Csharp/Examples/StringsToCosts/strtoc.cs
@@ -1,1 +1,16 @@
-using HumanFactors;
+using HumanFactors.Geometry;
+using HumanFactors.GraphGenerator;
+using HumanFactors.SpatialStructures;
+using System;
+using System.Diagnostics;
+
+class StringToEdgeCost
+{
+	static public void Main(String[] args)
+	{
+		Debug.WriteLine("Hello, World!");
+
+
+		var plane_mesh = OBJLoader.LoadOBJ("plane.obj");
+	}
+}
