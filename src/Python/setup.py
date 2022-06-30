95c52d53521309955fe29a6b80b3704ba2fca967
cadop <cadop@users.noreply.github.com>
2022-06-30 07:33:15 -0400

    update path for building actions and install examples

--- src/Python/setup.py
+++ src/Python/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from glob import glob
 
+
 setup(name='humanfactors',
       version='0.2.2',
       description='Utilities to perform tasks',
@@ -9,7 +10,10 @@ setup(name='humanfactors',
       packages=find_packages(),
       data_files = [
             ('bin', glob("humanfactorspy/bin/*")),
-            ('Lib/site-packages/humanfactorspy/Example Models', glob("humanfactorspy/Example Models/*"))
+            # ('Lib/site-packages/humanfactorspy/bin', glob("humanfactorspy/bin/*")),
+            ('Lib/site-packages/humanfactorspy/Example Models', glob("humanfactorspy/Example Models/*")),
+            ('Lib/site-packages/humanfactorspy/Examples/Rhino', glob("humanfactorspy/Examples/*/*")),
+            ('Lib/site-packages/humanfactorspy/Examples', [x for x in glob("humanfactorspy/Examples/*") if 'Rhino' not in x])
       ],
       install_requires=[
             'numpy',
