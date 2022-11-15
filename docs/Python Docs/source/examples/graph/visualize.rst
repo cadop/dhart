f90dc88581d7218393664f562047eda976ddc40a
cadop <cadop@users.noreply.github.com>
2022-11-15 09:27:00 -0500

    split graph generator examples into files

--- docs/Python Docs/source/examples/graph/visualize.rst
+++ docs/Python Docs/source/examples/graph/visualize.rst
@@ -6,3 +6,14 @@ Visualize the Graph
 
 .. automodule:: dhart.Examples.GraphGeneratorPlot
     :members:
+
+## Slope Settings 
+
+.. automodule:: dhart.Examples.GraphGeneratorSlope
+    :members:
+
+## Determining Minimum Connections 
+
+.. automodule:: dhart.Examples.GraphGeneratorConnections
+    :members:
+    
--- src/Python/dhart/Examples/GraphGeneratorConnections.py
+++ src/Python/dhart/Examples/GraphGeneratorConnections.py
@@ -1,11 +1,14 @@
 """
 
+Using the min_connections parameter we can ensure nodes have a minimum number
+of out directions (out-degree), which can avoid walls/edges and prevent
+'islands' caused by single edge connections
+
 .. plot::
     :context: reset
+        
 
-    
     import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
 
     from dhart.geometry import LoadOBJ
     from dhart.raytracer import EmbreeBVH
@@ -14,6 +17,7 @@
 
     # Get a sample model path
     obj_path = dhart.get_sample_model("energy_blob_zup.obj")
+    print(obj_path)
 
     # Load the obj file
     obj = LoadOBJ(obj_path)
@@ -22,17 +26,23 @@
     bvh = EmbreeBVH(obj, True)
 
     # Set the graph parameters
-    start_point = (0, -20, 20)
-    spacing = (1, 1, 10)
+    start_point = (0, 0, 20)
+    spacing = (1, 1, 1)
     max_nodes = 5000
     up_step, down_step = 0.5, 0.5
-    up_slope, down_slope = 20, 20
+
     max_step_connections = 1
+
+    # Set a smaller threshold for slope
+    up_slope, down_slope = 5, 5
+
+    # Add a minimum out-degree for each node 
+    min_connections = 3
 
     # Generate the Graph
     graph = GenerateGraph(bvh, start_point, spacing, max_nodes,
                             up_step,up_slope,down_step,down_slope,
-                            max_step_connections, cores=-1)
+                            max_step_connections, min_connections, cores=-1)
 
     # Convert the graph to a CSR
     csr_graph = graph.CompressToCSR()
@@ -40,30 +50,10 @@
     # Get the nodes of the graph as a list of x,y,z,type,id tuples
     nodes = graph.getNodes()
 
-    # get the x,y,z coordinates of the nodes
+    # get the x,y,z coordinates of the nodes and set the color to the z value
     x = [ n[0] for n in nodes ] # x coordinate 
     y = [ n[1] for n in nodes ] # y coordinate 
     z = [ n[2] for n in nodes ] # z coordinate
-
-
-The graph can be plotted in 3D for illustrative purposes (although it is better to load
-an obj or use a proper engine like OpenGL).
-
-.. plot::
-    :context: close-figs
-
-    # Plot the graph in 3D
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.view_init(azim=-123, elev=15)
-
-    ax.scatter(x, y, z)
-    plt.show()
-
-From the top view, this can be plotted in x,y and color nodes by the z value
-
-.. plot::
-    :context: close-figs
 
     # Plot the graph
     plt.scatter(x, y, c=z, alpha=0.5)
@@ -72,7 +62,6 @@ From the top view, this can be plotted in x,y and color nodes by the z value
 """
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 from dhart.geometry import LoadOBJ
 from dhart.raytracer import EmbreeBVH
@@ -94,13 +83,19 @@ start_point = (0, 0, 20)
 spacing = (1, 1, 1)
 max_nodes = 5000
 up_step, down_step = 0.5, 0.5
-up_slope, down_slope = 20, 20
+
 max_step_connections = 1
+
+# Set a smaller threshold for slope
+up_slope, down_slope = 5, 5
+
+# Add a minimum out-degree for each node 
+min_connections = 3
 
 # Generate the Graph
 graph = GenerateGraph(bvh, start_point, spacing, max_nodes,
                         up_step,up_slope,down_step,down_slope,
-                        max_step_connections, cores=-1)
+                        max_step_connections, min_connections, cores=-1)
 
 # Convert the graph to a CSR
 csr_graph = graph.CompressToCSR()
@@ -108,25 +103,13 @@ csr_graph = graph.CompressToCSR()
 # Get the nodes of the graph as a list of x,y,z,type,id tuples
 nodes = graph.getNodes()
 
-# get the x,y,z coordinates of the nodes
+# get the x,y,z coordinates of the nodes and set the color to the z value
 x = [ n[0] for n in nodes ] # x coordinate 
 y = [ n[1] for n in nodes ] # y coordinate 
 z = [ n[2] for n in nodes ] # z coordinate
 
-
-# Plot the graph in 3D
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.view_init(azim=-123, elev=15)
-
-ax.scatter(x, y, z)
-plt.show()
-plt.clf()
-
-# Plot the graph and set color to the z value
+# Plot the graph
 plt.scatter(x, y, c=z, alpha=0.5)
 plt.show()
-plt.clf()
-
 
 
